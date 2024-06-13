333f14a6099426da257a45a20ad104ec24e6145e
ND-460 <128513821+ND-460@users.noreply.github.com>
2024-06-13 16:21:18 +0530

    custom modules

---
+++ DAY4/built-in-modules/custom-modules/app.js
@@ -0,0 +1,4 @@
+const {firstname,lastname} = require('./variable');
+const {greeting} = require('./fun');
+console.log(firstname,lastname);
+greeting(firstname,lastname);
---
+++ DAY4/built-in-modules/custom-modules/variable.js
@@ -0,0 +1,4 @@
+let fname = "neel";
+let lname = "patel";
+module.exports = {firstname: fname,lastname: lname};
+// console.log(module);
